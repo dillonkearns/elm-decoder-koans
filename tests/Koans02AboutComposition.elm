b8b9dffece85b72a9a3560f15170a002b1bda306
Dillon Kearns <dillon@dillonkearns.com>
2017-11-03 20:34:53 -0400

    Use more explicit syntax for composition koans.

--- tests/Koans02AboutComposition.elm
+++ tests/Koans02AboutComposition.elm
@@ -7,7 +7,7 @@ module Koans02AboutComposition exposing (suite)
     http://package.elm-lang.org/packages/elm-lang/core/5.1.1/Json-Decode#data-structures
 -}
 
-import Helpers exposing (blankCompositeDecoder, blankDecoder, testDecodesTo)
+import Helpers exposing (blankCompositeDecoder, blankDecoder, expectDecodesTo, me____x, testDecodesTo, x____replace)
 import Json.Decode as Decode
 import Utils.Test exposing (Test, describe, test)
 
@@ -15,20 +15,20 @@ import Utils.Test exposing (Test, describe, test)
 suite : Test
 suite =
     describe "About Composition"
-        [ testDecodesTo "Decode.list takes one of the primitive decoders as an argument and applies it to each element"
-            (Decode.list blankDecoder)
-            """[123, 456]"""
-            [ 123, 456 ]
-        , testDecodesTo "An empty list could be a list of anything so any primitive will do"
-            (blankCompositeDecoder Decode.string)
-            "[]"
-            []
-        , testDecodesTo "Decode.nullabe takes one of the primitive decoders and turns it into a Maybe"
-            (blankCompositeDecoder Decode.int)
-            "12345"
-            (Just 12345)
-        , testDecodesTo "Decode.nullabe will decode null into Nothing"
-            (blankCompositeDecoder Decode.int)
-            "null"
-            Nothing
+        [ test "Decode.list takes one of the primitive decoders as an argument and applies it to each element" <|
+            \() ->
+                [ 123, 456 ]
+                    |> expectDecodesTo (Decode.list blankDecoder) "[123, 456]"
+        , test "An empty list could be a list of anything so any primitive will fit there" <|
+            \() ->
+                []
+                    |> expectDecodesTo (blankCompositeDecoder Decode.string) "[]"
+        , test "Decode.nullabe takes one of the primitive decoders and turns it into a Maybe" <|
+            \() ->
+                Just 12345
+                    |> expectDecodesTo (blankCompositeDecoder Decode.int) "12345"
+        , test "Decode.nullable will decode null into Nothing" <|
+            \() ->
+                x____replace me____x
+                    |> expectDecodesTo (Decode.nullable Decode.int) "null"
         ]
